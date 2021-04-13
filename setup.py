081200a035c18b06481256a6fa99cbfe737b5bad
Daniel Antoš <dan.antos@seznam.cz>
2021-04-13 22:49:53 +0200

    feat: initial setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='NanoTank',
+   version='0.0.0',
+   description='aquarium controller',
+   author='Daniel Antoš'
+ #  author_email='foomail@foo.com',
+ #  packages=['foo'],  #same as name
+ #  install_requires=['wheel', 'bar', 'greek'], #external packages as dependencies
+)
