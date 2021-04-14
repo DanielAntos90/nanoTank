7a3fee41a09f7020f746fcfb713989f04b8f48ab
DanielAntos90 <74924681+DanielAntos90@users.noreply.github.com>
2021-04-14 10:35:40 +0200

    fix: setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,3 @@
 from setuptools import setup
 
-setup(
-   name='NanoTank',
-   version='0.1.0',
-   description='aquarium controller',
-   author='Daniel Antoš'
- #  author_email='foomail@foo.com',
- #  packages=['foo'],  #same as name
- #  install_requires=['wheel', 'bar', 'greek'], #external packages as dependencies
-)
+setuptools.setup()
